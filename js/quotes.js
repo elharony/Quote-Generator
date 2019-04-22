96783a8df5c31ee33d25892251b0aea880f73216
elharony <yahya@elharony.com>
2019-04-22 06:07:12 +0200

    static: Add 'quotes' file with some data

---
+++ js/quotes.js
@@ -0,0 +1,137 @@
+const quotes = [
+    {
+      "quote": "Success is not final; failure is not fatal: It is the courage to continue that counts.",
+      "author": "Winston S. Churchill",
+      "category": "Business"
+    },
+    {
+      "quote": "It is better to fail in originality than to succeed in imitation.",
+      "author": "Herman Melville",
+      "category": "Business"
+    },
+    {
+      "quote": "The road to success and the road to failure are almost exactly the same.",
+      "author": "Colin R. Davis",
+      "category": "Business"
+    },
+    {
+      "quote": "The best way to predict the future is to create it.",
+      "author": "Peter Drucker",
+      "category": "Business"
+    },
+    {
+      "quote": "Rule No. 1: Never lose money; rule No. 2: Don’t forget rule No. 1.",
+      "author": "Warren Buffett",
+      "category": "Business"
+    },
+    {
+      "quote": "Change before you have to.",
+      "author": "Jack Welch",
+      "category": "Business"
+    },
+    {
+      "quote": "Your most unhappy customers are your greatest source of learning",
+      "author": "Bill Gates",
+      "category": "Business"
+    },
+    {
+      "quote": "There is only one boss. The customer. And he can fire everybody in the company from the chairman on down, simply by spending his money somewhere else.",
+      "author": "Sam Walton",
+      "category": "Business"
+    },
+    {
+      "quote": "A business that makes nothing but money is a poor business.",
+      "author": "Henry Ford",
+      "category": "Business"
+    },
+    {
+      "quote": "Choose a job that you love, and you will never have to work a day in your life.",
+      "author": "Confucius",
+      "category": "Business"
+    },
+    {
+      "quote": "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do.",
+      "author": "Steve Jobs",
+      "category": "Business"
+    },
+    {
+      "quote": "All our dreams can come true – if we have the courage to pursue them.",
+      "author": "Walt Disney",
+      "category": "Business"
+    },
+    {
+      "quote": "Business opportunities are like buses, there’s always another one coming.",
+      "author": "Richard Branson",
+      "category": "Business"
+    },
+    {
+      "quote": "There are two types of people who will tell you that you cannot make a difference in this world: those who are afraid to try and those who are afraid you will succeed.",
+      "author": "Ray Goforth",
+      "category": "Business"
+    },
+    {
+      "quote": "I learned to never kick someone when they’re down. Everyone makes mistakes, and some are real whoppers. But that makes them whopping opportunities, too.",
+      "author": "Jack Welsh",
+      "category": "Business"
+    },
+    {
+      "quote": "You need to have a culture instead of a payroll so that people watch themselves. What does this? Not money, but enhanced self-esteem.",
+      "author": "Steve Wynn",
+      "category": "Business"
+    },
+    {
+      "quote": "Successful people do what unsuccessful people are not willing to do. Don’t wish it were easier; wish you were better.",
+      "author": "Jim Rohn",
+      "category": "Business"
+    },
+    {
+      "quote": "Stop chasing the money and start chasing the passion.",
+      "author": "Tony Hsieh",
+      "category": "Business"
+    },
+    {
+      "quote": "Would you like me to give you a formula for success? It’s quite simple, really: Double your rate of failure. You are thinking of failure as the enemy of success. But it isn’t at all. You can be discouraged by failure, or you can learn from it, so go ahead and make mistakes. Make all you can. Because remember that’s where you will find success.",
+      "author": "Thomas J. Watson",
+      "category": "Business"
+    },
+    {
+      "quote": "People who succeed have momentum. The more they succeed, the more they want to succeed, and the more they find a way to succeed. Similarly, when someone is failing, the tendency is to get on a downward spiral that can even become a self-fulfilling prophecy.",
+      "author": "Tony Robbins",
+      "category": "Business"
+    },
+    {
+      "quote": "Don’t be distracted by criticism. Remember–the only taste of success some people get is to take a bite out of you.",
+      "author": "Zig Ziglar",
+      "category": "Business"
+    },
+    {
+      "quote": "The secret of success is to do the common thing uncommonly well.",
+      "author": "John D. Rockefeller Jr.",
+      "category": "Business"
+    },
+    {
+      "quote": "You know you are on the road to success if you would do your job, and not be paid for it.",
+      "author": "Oprah Winfrey",
+      "category": "Business"
+    },
+    {
+      "quote": "If someone is out in the desert walking around, they’re going to be thirsty. You just have to ask them what they want to drink.",
+      "author": "Arthur Blank",
+      "category": "Business"
+    },
+    {
+      "quote": "I never did anything worth doing by accident, nor did any of my inventions come indirectly through accident, except the phonograph. No, when I have fully decided that a result is worth getting, I go about it, and make trial after trial, until it comes.",
+      "author": "Thomas Edison",
+      "category": "Business"
+    },
+    {
+      "quote": "If you just work on stuff that you like and you’re passionate about, you don’t have to have a master plan with how things will play out.",
+      "author": "Mark Zuckerberg",
+      "category": "Business"
+    },
+    {
+      "quote": "Character cannot be developed in ease and quiet. Only through experience of trial and suffering can the soul be strengthened, ambition inspired, and success achieved.",
+      "author": "Helen Keller",
+      "category": "Business"
+    }
+]
