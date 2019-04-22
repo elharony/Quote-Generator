1eabc77cc3e29803463621562820b1cb0d192b34
elharony <yahya@elharony.com>
2019-04-22 06:07:38 +0200

    Generate a random quote when clicking on a button

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const quoteElem = document.querySelector('#quote')
+const generateBtn = document.querySelector('#generate')
+
+
+generateBtn.addEventListener('click', () => {
+  getRandomQuote()
+})
+
+getRandomQuote = () => {
+  let rand = getRandomInt(0, 27)
+  console.log(rand)
+  let selectedQuote = quotes[rand]
+  quoteElem.innerHTML = selectedQuote.quote
+  console.log(selectedQuote)
+}
+
+// SOF: https://stackoverflow.com/a/1527820/5560399
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
