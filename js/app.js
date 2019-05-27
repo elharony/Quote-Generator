e2ab3a7b006fd067e4db5d6c371bf3bfdd43b9d8
elharony <yahya@elharony.com>
2019-05-27 10:13:15 +0200

    Improve the documentation of the JS code

--- js/app.js
+++ js/app.js
@@ -1,18 +1,142 @@
-const quoteElem = document.querySelector('#quote')
-const authorElem = document.querySelector('#author')
-const generateBtns = document.querySelectorAll('.generate-btn')
 const main = document.querySelector('main')
+
+/* Quote Body */
 const quotesContainer = document.querySelector('.quotes-container')
+
+/* Quote Options */
+const generateBtns = document.querySelectorAll('.generate-btn')
+const quoteCounter = document.querySelector('#quote-counter')
+const showAll = document.querySelector('#show-all')
+let currentQuoteIndex
+let currentQuoteCounter = quoteCounter.options[quoteCounter.selectedIndex].value
+
+/* Gridview */
 const quotesWrapper = document.querySelector('.quotes-wrapper')
 const quotesGrid = document.querySelector('.quotes')
-const showAll = document.querySelector('#show-all')
-const quoteCounter = document.querySelector('#quote-counter')
-let currentQuoteIndex;
-let currentQuoteCounter = quoteCounter.options[quoteCounter.selectedIndex].value;
+
+
+
+
+/************************************************************
+ * ############
+ * Program Flow
+ * ############
+ * 1) `init`: Initial Startup Quote
+ * 2) Add `click` event to quote `category` button
+ * 3) `getRandomInt`: Generate `random` integer number
+ * 4) `getRandomQuote`: Loop on `howMuch` quotes, call `getRandomInt`, avoid `category` duplication, call `displayQuote`
+ * 5) `displayQuote`: Build the quote DOM, according to the given `random` quote index
+ * 6) Add `click` event to `Show All` grid button, start the enter animation of the `quotesWrapper`
+ * 7) `hideGrid`: Click on the `X` button, start the exit animation of the `quotesQrapper`
+ ***********************************************************/
+
+
 
 
 /**
- * `Click` Event Listener
+ * Add `click` event to each `generateBtns` element (i.e. `Random`, `Business`)
+ * Invoke the `getRandomQuote`
+ * Pass the `data-value` attribute
+ */
+for(let i = 0; i < generateBtns.length; i++) {
+  generateBtns[i].addEventListener('click', () => {
+    getRandomQuote(generateBtns[i].getAttribute('data-value'), currentQuoteCounter)
+  })
+}
+
+
+/**
+ * Update the required quotes number (i.e. `howMuch` parameter)
+ */
+function updateCounter() {
+  currentQuoteCounter = quoteCounter.options[quoteCounter.selectedIndex].value
+}
+
+
+/**
+ * @returns Random integer within the "min" and "max" range
+ * SOF: https://stackoverflow.com/a/1527820/5560399
+ */
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+
+/**
+ * Get the required `category` and `howMany` quotes to be displayed
+ * Generate `random` quotes indexes
+ * Avoid Duplication
+ * If it went well, invoke the `displayQuote` to show random quote(s)
+ * If not, run the function again!
+ */
+getRandomQuote = (category, howMany) => {
+
+  // Empty before adding new quotes
+  quotesContainer.innerHTML = ''
+
+  for(let i=0; i < howMany; i++) {
+    let rand = getRandomInt(0, 58)
+
+    // **Avoid Duplication** If we get the same quote, get another one
+    if(rand === currentQuoteIndex) {
+      return getRandomQuote(category, currentQuoteCounter)
+    }
+
+    /**
+     * Get Quotes based on the selected Category
+     * If `category` != 'All',
+     * Do we get the same category randomly?
+     * If so, display that quote
+     * If not, recursive!
+     */
+    if(category == 'All') {
+      displayQuote(rand)
+    } else {
+      if(quotes[rand].category == category) {
+        displayQuote(rand)
+      } else {
+        return getRandomQuote(category, currentQuoteCounter)
+      }
+    }
+  }
+}
+
+
+/**
+ * Create the DOM to show a quote, according to the given quote index
+ */
+displayQuote = (randomQuoteIndex) => {
+
+  // Update current index
+  currentQuoteIndex = randomQuoteIndex
+  
+  /**
+   * Cache the selected quote - `quotes[randomQuoteIndex]`
+   * Display the `quote` text
+   * Display the `author` text
+   */
+  let selectedQuote = quotes[randomQuoteIndex]
+  let quoteElem = document.createElement('h2')
+  quoteElem.classList.add('quote')
+  let authorElem = document.createElement('p')
+  authorElem.classList.add('author')
+  quoteElem.innerHTML = selectedQuote.quote
+  authorElem.innerHTML = `- ${selectedQuote.author}`
+
+  quotesContainer.appendChild(quoteElem)
+  quotesContainer.appendChild(authorElem)
+
+  /**
+   * Change the background
+   */
+  main.style.background = `#ffffff url(./img/${selectedQuote.category.toLowerCase()}.jpg) center`
+  main.style.backgroundSize = 'cover'
+}
+
+
+/**
  * Animate the `quotesGrid` to enter the UI
  * Display all available `quotes`
  */
@@ -43,109 +167,19 @@ showAll.addEventListener('click', () => {
   }
 })
 
+
 /**
- * Hide `all quotes` grid
+ * Animate the `quotesGrid` to exit the UI
+ * DON'T remove the current displayed `quotes`
  */
 function hideGrid() {
   quotesWrapper.classList.remove('animate')
 }
 
+
 /**
- * Update `quote-counter` value
+ * Startup Quote
  */
-function updateCounter() {
-  currentQuoteCounter = quoteCounter.options[quoteCounter.selectedIndex].value
-}
-
-
-
-// Click Event
-for(let i = 0; i < generateBtns.length; i++) {
-
-  /**
-   * Add `click` event to each `generateBtns`
-   * Invoke the `getRandomQuote`
-   * Pass the `data-value` attribute
-   **/
-  generateBtns[i].addEventListener('click', () => {
-    getRandomQuote(generateBtns[i].getAttribute('data-value'), currentQuoteCounter)
-  })
-}
-
-
-getRandomQuote = (category, howMany) => {
-
-  // Empty before adding new quotes
-  quotesContainer.innerHTML = ''
-
-  for(let i=0; i < howMany; i++) {
-    let rand = getRandomInt(0, 58)
-
-    // If we get the same quote, get another one
-    if(rand === currentQuoteIndex) {
-      return getRandomQuote(category, currentQuoteCounter)
-    }
-
-    // Get Quotes based on the selected Category
-    if(category == 'All') {
-      displayQuote(rand)
-    } else {
-
-      /**
-       * If `category` != 'All',
-       * Do we get the same category randomly?
-       * If so, display that quote
-       * If not, recursive!
-       */
-      if(quotes[rand].category == category) {
-        displayQuote(rand)
-      } else {
-        return getRandomQuote(category, currentQuoteCounter)
-      }
-    }
-  }
-
-}
-
-
-displayQuote = (randomQuoteIndex) => {
-
-   // Update current index
-   currentQuoteIndex = randomQuoteIndex
-  
-  /**
-   * Cache the selected quote - `quotes[randomQuoteIndex]`
-   * Display the `quote` text
-   * Display the `author` text
-   */
-   let selectedQuote = quotes[randomQuoteIndex]
-   let quoteElem = document.createElement('h2')
-   quoteElem.classList.add('quote')
-   let authorElem = document.createElement('p')
-   authorElem.classList.add('author')
-   quoteElem.innerHTML = selectedQuote.quote
-   authorElem.innerHTML = `- ${selectedQuote.author}`
-
-   quotesContainer.appendChild(quoteElem)
-   quotesContainer.appendChild(authorElem)
-
-   /**
-    * Change the background
-    */
-   main.style.background = `#ffffff url(./img/${selectedQuote.category.toLowerCase()}.jpg) center`
-   main.style.backgroundSize = 'cover'
-}
-
-
-// SOF: https://stackoverflow.com/a/1527820/5560399
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-
-// Startup Quote
 (function init() {
   getRandomQuote('All', currentQuoteCounter)
 })()
